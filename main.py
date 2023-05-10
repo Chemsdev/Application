09d533d08a6c41c255591ef90ae2cf6ec34bfc18
chemsdine777 <104915350+chemsdine777@users.noreply.github.com>
2023-05-10 12:10:00 +0200

    debug api

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import streamlit as st
-from bdd import create_tables, data_insert, get_data_from_api
+import pandas as pd
 import requests
 
 
+# =======================================================================>
+# Fonction pour récupérer les données depuis l'API.
+def send_data_to_api(data, url="http://localhost:8000/data/post"):
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        st.success("Données insérées avec succès.")
+    else:
+        st.error("Erreur lors de l'insertion des données.")
+# =======================================================================>
+# Fonction pour afficher les données depuis l'API.
+def get_data_from_api(url="http://localhost:8000/data/get"):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json().get("data")
+        st.success("Données récupérées avec succès.")
+        return data
+    else:
+        return st.error("Erreur lors de la récupération des données.")
+# =======================================================================>
+
 def main():
     
-    # ======================= DATA =============================>
-    create_tables(table_name_1="table_A", table_name_2="table_B")
-    data_insert(table_name_1="table_a", table_name_2="table_b")
+    data = {
+        "feature_0": st.text_input("Saisir feature",     key=1),
+        "feature_1": st.text_input("Saisir feature",     key=2),
+        "feature_2": st.text_input("Saisir feature",     key=3),
+        "y_pred"   : st.text_input("Saisir predictions", key=4)
+    }
     
-    # ======================= API ==============================>
-    data = get_data_from_api()
-    st.write("Données récupérées depuis la base de données :")
-    if data:
-        st.write(str(data))
-    else:
-        st.write("Aucune donnée disponible.")
-        
+    if st.button("envoyer"):
+        send_data_to_api(data)
+        get_data_from_api()
+
 main()
 
