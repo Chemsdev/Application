9a5a32db2ee5d0c5aab2794d72ae2e924ca7c8e5
Ounissi Chems <chemscreative@gmail.com>
2023-05-24 14:54:59 +0200

    change

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
-from functions import send_data_to_api, create_tables, traitement_formualaire, background_front
+from functions import send_data_to_api, create_tables, traitement_formualaire, background_front, encart_prediction
 
 
 
 def main():
     
     # ======================= FRONT =========================== #
-    background_front(url="https://wallpapercave.com/wp/wp3460663.jpg")
+    background_front(url="https://wallpaperaccess.com/full/1704480.jpg")
     
     # ======================== SQL ============================ #
     create_tables(table_name_1="features", table_name_2="predictions")
--- pages/consultation.py
+++ pages/consultation.py
@@ -10,7 +10,7 @@ def data():
     features, predictions = get_data_from_api()
     
     # ========================== PAGE ================================>
-    background_front(url="https://wallpapercave.com/wp/wp3460663.jpg")
+    background_front(url="https://wallpaperaccess.com/full/1704480.jpg")
     data = columns_DataFrame(data1=features, data2=predictions)
     st.title("Consultation des données")
     st.markdown("Les données")
