95356de0534fa9cfee0f0f728dcc388bdaa97f21
Ounissi Chems <chemscreative@gmail.com>
2023-06-02 00:03:37 +0200

    g

--- pages/03_Insights.py
+++ pages/03_Insights.py
@@ -12,8 +12,10 @@ from functions import background_front, style_text
 # DATASET ==============================================================
 background_front(url="https://nano.scrolller.com/abstract-wallpaper-9nyw4ngmg1.jpg")
 df = pd.read_csv("data/gsearch_jobs.csv")
+top_skills_df = pd.read_csv("data/top_skills_df.csv")
 
-graph = st.sidebar.radio(label='Pages graphs', options=('1', '2', '3'))
+
+graph = st.sidebar.radio(label='Pages graphs', options=('1', '2', '3', '4'))
 
 if graph == '1':
     style_text(title='Top 10 Titres de poste', size=30)
@@ -64,6 +66,24 @@ elif graph == '3':
     fig.data[0].marker.line.color='gray'
     fig.update_layout(
         font=dict(size=20,family="Franklin Gothic"))
+    st.plotly_chart(fig)
+    
+elif graph == '4':
+    st.title('Top 10 Compétences')
+    
+    fig = px.treemap(top_skills_df, path=['skill'], values='number_of_postings',
+                  color='avg_yearly_salary', color_continuous_scale='Blues')
+
+    # fig.update_traces(hovertemplate='Skill: %{label} <br> \ Postings: %{value} <br> Average yearly salary: %{color}<extra></extra>')
+
+    fig.update_layout(
+        title_text="Salaires associées aux 10 compétences<br>de Data Analystes les plus populaires", 
+        title_x=0.1, title_font_size = 24, font_color="white",
+        paper_bgcolor="#444444", 
+        coloraxis_colorbar=dict(title="Salaire annuel moyen")
+    )
+
+    # Render the plotly figure using Streamlit
     st.plotly_chart(fig)
     
     
